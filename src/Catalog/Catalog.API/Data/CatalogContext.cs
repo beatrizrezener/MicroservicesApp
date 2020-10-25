e6df54836be8709e8d8264e2a7c144b0b92d68a3
Beatriz Rezener <beatrizrezener@gmail.com>
2020-10-25 00:23:45 -0300

    Seeding Products Collection

--- src/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -14,6 +14,7 @@ namespace Catalog.API.Data
             var database = client.GetDatabase(settings.DatabaseName);
 
             Products = database.GetCollection<Product>(settings.CollectionName);
+            CatalogContextSeed.SeedData(Products);
         }
 
         public IMongoCollection<Product> Products { get; }
---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        public static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Product A",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "imageA.png",
+                    Price = 950.00M,
+                    Category = "Smartphone"
+                },
+                new Product()
+                {
+                    Name = "Product B",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "imageB.png",
+                    Price = 840.00M,
+                    Category = "Smartphone"
+                },
+                                new Product()
+                {
+                    Name = "Product C",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "imageC.png",
+                    Price = 350.00M,
+                    Category = "Smartphone"
+                },
+            };
+        }
+    }
+}
