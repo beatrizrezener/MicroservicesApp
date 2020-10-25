87046a9a771805ba6a36b5b4101a735b378ec43c
Beatriz Rezener <beatrizrezener@gmail.com>
2020-10-24 23:49:51 -0300

    Fix product class name

--- src/Catalog/Catalog.API/Entities/Product.cs
+++ src/Catalog/Catalog.API/Entities/Product.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 namespace Catalog.API.Entities
 {
-    public class Products
+    public class Product
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
